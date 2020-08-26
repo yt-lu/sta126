d2ec93989d9d2e2e337c05f3516cdcf0d9f59f2a
yt-lu <63565317+yt-lu@users.noreply.github.com>
2020-08-26 12:06:43 -0400

    Add files via upload

--- nicer.R
+++ nicer.R
@@ -25,14 +25,13 @@ freqpoly <- function (x, breaks, pch = 1, col = 'black', xlab = NULL, ylab = NUL
 }
 
 # Lollipop 
-lollipop <- function (x, cex = 1.5, lwd = 0.8, dcol = 'black', bcol = 'black',
-                      xlab = NULL, ylab = "Frequency", main = NULL,
-                      xlim = NULL, ylim = Null, ...){
+lollipop <- function (x, cex = 1.5, lwd = 1, dcol = 'black', bcol = 'black',
+                      ylab = "Frequency", main = NULL, ...){
   a <- as.data.frame(x)
   names(a) <- c("factors", "counts")
   n <- nrow(a)
-  plot(1:n, a$counts, pch = 16, cex = cex, xlab = xlab, ylab = ylab, 
-       xaxt = "n", main = main, col = dcol, xlim = xlim, ylim = ylim)
+  plot(1:n, a$counts, pch = 16, cex = cex, ylab = ylab, 
+       xaxt = "n", main = main, col = dcol, ...)
   segments(1:n, rep(0, n), 1:n, a$counts, lwd = lwd, col = bcol)
   axis(1, at = 1:n, labels = a$factors)
 }
